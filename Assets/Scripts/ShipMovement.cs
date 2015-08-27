b625776ff246ab81fba2f3f6ce00b3207f47daa1
Rufus125 <Rufus125@users.noreply.github.com>
2015-08-27 12:32:37 +0300

    Added the boat image
    The ship moves
    Added a background image of the ocean. Must be replaced

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -2,14 +2,22 @@
 using System.Collections;
 
 public class ShipMovement : MonoBehaviour {
+	
+	private Rigidbody2D rigidBody;
+	public float speed = 15;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
 
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.RightArrow)){
-
-		}
-		if(Input.GetKeyDown(KeyCode.LeftArrow)){
-			
+				rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+			}
+	if(Input.GetKeyDown(KeyCode.LeftArrow)){
+		rigidBody.velocity = new Vector2(-speed, rigidBody.velocity.y);
 		}
 	}
 }
