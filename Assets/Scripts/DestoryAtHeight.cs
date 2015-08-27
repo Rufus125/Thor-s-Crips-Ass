3dfbbc3b5e18a348123afba16bc60efe89f6b223
Thomas <sneakyshuffle@gmail.com>
2015-08-27 16:01:15 +0300

    destory objects when off screen
    added script to prefabs so when they are below screen height they are
    destoryed

---
+++ Assets/Scripts/DestoryAtHeight.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryAtHeight : MonoBehaviour {
+
+    public float height;
+
+    void Update() {
+
+        if(transform.position.y < height)
+            Destroy( gameObject ); 
+    }
+}
