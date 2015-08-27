2f828e55e09a672cfd72186b060e65f879f21aa7
Rufus125 <Rufus125@users.noreply.github.com>
2015-08-27 12:05:00 +0300

    Initial commit

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			
+		}
+	}
+}
